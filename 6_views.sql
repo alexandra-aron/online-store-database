c392e9318af4d4ceeef458159941ba99952b19b7
Alexandra Aron <aron.cristina.alexandra@gmail.com>
2026-05-29 00:00:40 +0300

    Update 6_views.sql
    added more views

--- 6_views.sql
+++ 6_views.sql
@@ -1,8 +1,8 @@
 -- 6. view-uri
 --------------------------------------------------------------
 
-
 -- view pentru afisarea rapida a comenzilor complete
+
 CREATE VIEW view_comenzi_rapide AS
 SELECT
     co.id_comanda,
@@ -24,11 +24,12 @@ JOIN produs p
 JOIN categorie cat
     ON p.categorie_id = cat.id_categorie;
 
-
 SELECT * FROM view_comenzi_rapide;
 
 
+
 -- produse care nu au fost vandute deloc
+
 CREATE VIEW produse_nevandute AS
 SELECT
     p.id_produs,
@@ -39,5 +40,116 @@ LEFT JOIN produse_comandate pc
     ON p.id_produs = pc.id_produs
 WHERE pc.id_produs IS NULL;
 
-
 SELECT * FROM produse_nevandute;
+
+
+
+----produse scumpe
+
+---v1
+CREATE VIEW produse_scumpe1 AS
+WITH medie AS
+	(SELECT AVG(pret) AS medie_pret
+	FROM produs
+  )
+SELECT p.nume_produs, p.pret
+FROM produs p , medie m
+WHERE p.pret > m.medie_pret;
+
+SELECT * FROM produse_scumpe1;
+
+--v2
+CREATE VIEW produse_scumpe2 AS
+WITH medie AS
+	(SELECT AVG(pret) AS medie_pret
+	FROM produs
+  )
+SELECT p.nume_produs, p.pret
+FROM produs p
+CROSS JOIN medie m
+WHERE p.pret > m.medie_pret;
+
+SELECT * FROM produse_scumpe2;
+
+
+
+
+--utilizatori care au cheltuit peste_media_cheltuielilor
+
+--v1
+CREATE VIEW peste_media_cheltuielilor_1 AS
+WITH total_pret AS
+		(SELECT u.id_utilizator, u.nume_utilizator, SUM(pc.cantitate*pc.pret) AS pret_total
+		FROM utilizator u
+		JOIN comanda co ON u.id_utilizator = co.id_utilizator 
+		JOIN produse_comandate pc ON co.id_comanda = pc.id_comanda
+		GROUP BY u.id_utilizator, u.nume_utilizator
+  ),
+medie AS (
+    SELECT AVG(pret_total) AS media_totalurilor
+	  FROM total_pret
+  )
+SELECT tp.nume_utilizator, tp.pret_total
+FROM total_pret tp
+CROSS JOIN medie m
+WHERE tp.pret_total > m.media_totalurilor;
+
+SELECT * FROM peste_media_cheltuielilor_1;
+
+--v2
+CREATE VIEW peste_media_cheltuielilor_2 AS
+WITH total_pret AS
+		(SELECT u.id_utilizator, u.nume_utilizator, SUM(pc.cantitate*pc.pret) AS pret_total
+		FROM utilizator u
+		JOIN comanda co ON u.id_utilizator = co.id_utilizator 
+		JOIN produse_comandate pc ON co.id_comanda = pc.id_comanda
+		GROUP BY u.id_utilizator, u.nume_utilizator
+  )
+SELECT tp.nume_utilizator, tp.pret_total
+FROM total_pret tp
+WHERE tp.pret_total > ( SELECT AVG(pret_total) FROM total_pret );
+
+SELECT * FROM peste_media_cheltuielilor_2;
+
+
+
+
+--produse mai vandute decat media
+
+--v1
+CREATE VIEW produse_mai_vandute_decat_media_1 
+    AS
+WITH total_cantitate_produs AS 
+		(SELECT p.id_produs, p.nume_produs,
+			SUM(pc.cantitate) AS cantitate_totala
+		FROM produs p
+		JOIN produse_comandate pc ON p.id_produs = pc.id_produs
+		GROUP BY p.id_produs, p.nume_produs),
+
+medie AS (SELECT AVG(cantitate_totala) AS medie_cantitate
+		FROM total_cantitate_produs)
+
+SELECT  t.nume_produs, t.cantitate_totala
+FROM total_cantitate_produs t
+CROSS JOIN medie m
+WHERE t.cantitate_totala > m.medie_cantitate;
+
+SELECT * FROM produse_mai_vandute_decat_media_1;
+
+--v2
+CREATE VIEW produse_mai_vandute_decat_media_2
+    AS
+WITH total_cantitate_produs AS 
+		(SELECT p.id_produs, p.nume_produs,
+			SUM(pc.cantitate) AS cantitate_totala
+		FROM produs p
+		JOIN produse_comandate pc ON p.id_produs = pc.id_produs
+		GROUP BY p.id_produs, p.nume_produs),
+
+medie AS (SELECT AVG(cantitate_totala) AS medie_cantitate
+		FROM total_cantitate_produs)
+SELECT id_produs, nume_produs
+FROM total_cantitate_produs
+WHERE cantitate_totala > (SELECT medie_cantitate FROM medie);
+
+SELECT * FROM produse_mai_vandute_decat_media_2;
