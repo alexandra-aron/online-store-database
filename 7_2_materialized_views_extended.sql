bf7193ec9d1e14023570a36e2fc71d24c1d9eb16
Alexandra Aron <aron.cristina.alexandra@gmail.com>
2026-05-29 00:21:35 +0300

    Update 7_2_materialized_views_extended.sql
    more mvs

--- 7_2_materialized_views_extended.sql
+++ 7_2_materialized_views_extended.sql
@@ -147,3 +147,212 @@ REFRESH MATERIALIZED VIEW comenzi_finalizate;
 
 
 
+--8. top clienti
+
+CREATE MATERIALIZED VIEW top_clienti_mv AS 
+SELECT u.id_utilizator, u.nume_utilizator, 
+ 		SUM(pc.cantitate * pc.pret) AS total_cheltuit,
+		COUNT(DISTINCT pc.id_comanda) AS numar_comenzi
+		
+		FROM utilizator u
+		JOIN comanda co ON u.id_utilizator = co.id_utilizator
+ 		JOIN produse_comandate pc ON co.id_comanda = pc.id_comanda
+ 		GROUP BY u.id_utilizator , u.nume_utilizator;
+		
+SELECT * FROM  top_clienti_mv
+ORDER BY total_cheltuit DESC;
+
+REFRESH MATERIALIZED VIEW top_clienti_mv;
+
+
+
+
+
+-- 9. MV vanzari pe zile 
+
+CREATE MATERIALIZED VIEW nr_vanzari_pe_zile AS 
+	SELECT co.data_creare AS zi , 
+	COUNT(DISTINCT pc.id_comanda) AS numar_comenzi, 
+	SUM(pc.cantitate * pc.pret) AS suma_total_vanzari
+	FROM comanda co
+	JOIN produse_comandate pc ON co.id_comanda = pc.id_comanda
+	GROUP BY co.data_creare
+	ORDER BY zi DESC;
+
+SELECT * FROM nr_vanzari_pe_zile;
+
+REFRESH MATERIALIZED VIEW nr_vanzari_pe_zile;
+
+
+
+
+--10. produse necomandate
+
+CREATE MATERIALIZED VIEW produse_necomandate 
+	AS
+SELECT p.id_produs, p.nume_produs 
+FROM produs p
+LEFT JOIN produse_comandate pc ON p.id_produs = pc.id_produs
+WHERE pc.id_produs IS NULL;
+
+SELECT * FROM produse_necomandate;
+
+REFRESH MATERIALIZED VIEW produse_necomandate;
+
+
+
+
+
+--- 11. MV stoc_critic
+
+CREATE MATERIALIZED VIEW mv_stoc_critic 
+	AS
+SELECT cat.id_categorie, cat.nume_categorie, p.nume_produs,p.stoc, SUM(pc.cantitate) AS cantitate_vanduta_total
+FROM categorie cat
+JOIN produs p ON cat.id_categorie = p.categorie_id
+JOIN produse_comandate pc ON p.id_produs = pc.id_produs 
+WHERE p.stoc < 15
+GROUP BY cat.id_categorie, cat.nume_categorie, p.nume_produs,p.stoc;
+
+SELECT * FROM mv_stoc_critic;
+
+REFRESH MATERIALIZED VIEW  mv_stoc_critic;
+
+
+
+
+
+--12. MV produse premium
+
+CREATE MATERIALIZED VIEW produse_premium 
+	AS
+WITH total_venit AS (
+	SELECT p.id_produs, p.nume_produs, SUM(pc.cantitate*pc.pret) AS venit_total
+	FROM produs p
+	JOIN produse_comandate pc ON p.id_produs = pc.id_produs
+	GROUP BY p.id_produs, p.nume_produs
+	),
+  medie AS (
+	SELECT AVG(venit_total) AS medie_venit 
+	FROM total_venit
+	)
+
+SELECT id_produs, nume_produs
+FROM total_venit tv
+CROSS JOIN medie m
+WHERE tv.venit_total > m.medie_venit;
+
+SELECT * FROM produse_premium;
+
+REFRESH MATERIALIZED VIEW  produse_premium;
+
+
+
+
+
+--- 13. clienti premium
+
+CREATE MATERIALIZED VIEW clienti_premium 
+	AS
+WITH  total_venit_si_comenzi_distincte AS ( SELECT u.id_utilizator, u.nume_utilizator, 
+											        SUM(pc.cantitate * pc.pret) AS total_cheltuit,
+													COUNT(DISTINCT(co.id_comanda)) AS nr_comenzi
+											FROM utilizator u
+											JOIN comanda co ON u.id_utilizator = co.id_utilizator
+											JOIN produse_comandate pc ON co.id_comanda = pc.id_comanda
+											GROUP BY u.id_utilizator, u.nume_utilizator
+	),
+medie_cheltuieli AS (
+	SELECT AVG(total_cheltuit) AS medie 
+	FROM total_venit_si_comenzi_distincte
+	)
+SELECT t.id_utilizator, t.nume_utilizator, t.total_cheltuit, t.nr_comenzi
+FROM total_venit_si_comenzi_distincte t
+CROSS JOIN medie_cheltuieli m
+WHERE t.total_cheltuit > m.medie AND t.nr_comenzi >=2;
+
+SELECT * FROM clienti_premium;
+
+REFRESH MATERIALIZED VIEW  clienti_premium;
+
+
+
+
+--- 14. MV categorii_profitabile 
+
+--v1
+CREATE MATERIALIZED VIEW categorii_profitabile1 
+	AS
+WITH categorie_statistici1 AS 
+		(SELECT cat.id_categorie, cat.nume_categorie, 
+		        SUM(pc.cantitate* pc.pret) AS total_cheltuit,
+				SUM(pc.cantitate) AS total_produse_vandute,
+				SUM(pc.cantitate * pc.pret) /  NULLIF(COUNT(DISTINCT p.id_produs),0) AS profit_mediu_per_produs
+		FROM categorie cat
+		JOIN produs p ON cat.id_categorie =p.id_categorie
+		JOIN produse_comandate pc ON pc.id_produs = p.id_produs
+		GROUP BY cat.id_categorie, cat.nume_categorie
+	)
+SELECT * 
+FROM  categorie_statistici1
+WHERE total_cheltuit > (SELECT AVG(total_cheltuit) FROM categorie_statistici1); 
+
+SELECT * FROM categorii_profitabile1;
+
+REFRESH MATERIALIZED VIEW  categorii_profitabile1;
+
+--- v2
+CREATE MATERIALIZED VIEW categorii_profitabile2 
+	AS
+WITH categorie_statistici2 AS 
+		(SELECT cat.id_categorie, cat.nume_categorie, 
+		        SUM(pc.cantitate* pc.pret) AS total_cheltuit,
+				SUM(pc.cantitate) AS total_produse_vandute,
+				SUM(pc.cantitate * pc.pret) / NULLIF(COUNT(DISTINCT p.id_produs),0) AS profit_mediu_per_produs
+		FROM categorie cat
+		JOIN produs p ON cat.id_categorie =p.id_categorie
+		JOIN produse_comandate pc ON pc.id_produs = p.id_produs
+		GROUP BY cat.id_categorie, cat.nume_categorie
+	),
+medie AS (
+	SELECT AVG(total_cheltuit) AS medie 
+	FROM categorie_statistici2
+	)
+SELECT * 
+FROM categorie_statistici2 cs
+CROSS JOIN medie m
+WHERE cs.total_cheltuit > m.medie;
+
+SELECT * FROM categorii_profitabile2;
+
+REFRESH MATERIALIZED VIEW  categorii_profitabil2;
+
+
+
+
+-- 15. MV produse rare si scumpe (WHERE c.cantitate_totala_vanduta < m.medie_cantitati AND c.pret > m.media_preturi)
+
+CREATE MATERIALIZED VIEW produse_rare_scumpe 
+	AS
+WITH cantitati_totale AS (SELECT p.id_produs, p.nume_produs,p.pret
+						 SUM(pc.cantitate) AS cantitate_totala_vanduta
+						 FROM produs p 
+						 JOIN produse_comandate pc ON p.id_produs = pc.id_produs 
+						 GROUP BY p.id_produs, p.nume_produs, p.pret
+	),
+medie AS (SELECT AVG(cantitate_totala_vanduta) AS medie_cantitati,
+ 				AVG(pret) AS media_preturi
+		   FROM cantitate_totala
+	)		
+SELECT c.id_produs, c.nume_produs, c.pret, c.cantitate_totala_vanduta
+FROM cantitati_totale c
+CROSS JOIN medie m
+WHERE c.cantitate_totala_vanduta < m.medie_cantitati AND c.pret > m.media_preturi;
+
+SELECT * FROM produse_rare_scumpe;
+
+REFRESH MATERIALIZED VIEW  produse_rare_scumpe;
+
+
+
+
